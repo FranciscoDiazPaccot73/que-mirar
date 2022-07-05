69629129331dbf796fd0b014d58d658c5b6ee65e
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2022-07-04 21:56:31 -0300

    fix analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import ReactGA from 'react-ga';
 
 import PageProvider from '../context/index';
 
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const trackingId = process.env.GA_ID ?? '';
+  ReactGA.initialize(trackingId);
+
   return (
     <QueryClientProvider client={queryClient}>
         <ChakraProvider>
