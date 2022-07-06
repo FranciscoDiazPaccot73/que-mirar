0f5a95068620546ffb2d2a2eaf77ae486a86fed4
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2022-07-06 09:29:09 -0300

    add meta tags

--- components/Seo/index.tsx
+++ components/Seo/index.tsx
@@ -3,9 +3,11 @@ import Head from 'next/head'
 interface Props {
   description: string,
   title: string,
+  logoUrl: string,
+  url: string,
 }
 
-const Seo = ({ description, title }: Props) => {
+const Seo = ({ description, title, logoUrl, url }: Props) => {
   return (
     <Head>
       <title>{title}</title>
@@ -15,9 +17,14 @@ const Seo = ({ description, title }: Props) => {
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content={title} />
       <meta property="twitter:card" content="summary" />
-      <meta property="twitter:creator" content="https://twitter.com/Pancho_xor" />
+      <meta property="twitter:creator" content="@Pancho_xor" />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={logoUrl} />
+      <meta property='og:title' content={title} />
+      <meta property='og:image' content={logoUrl} />
+      <meta property='og:description' content={description} />
+      <meta property='og:url' content={url} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <link rel="icon" href="/favicon.ico" />
     </Head>
@@ -27,6 +34,8 @@ const Seo = ({ description, title }: Props) => {
 Seo.defaultProps = {
   description: "Si no sabes que serie o pelicula empezar a ver, este es tu lugar",
   title: "¿Qué puedo ver?",
+  logoUrl: "https://i.ibb.co/yhYsNjq/que-puedo-ver-4.png",
+  url: "https://quepuedover.online",
 }
 
 export default Seo;
