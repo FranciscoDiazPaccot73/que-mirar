89a51efd3dec6d361191d114c6777312ce008aab
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2023-05-26 18:18:41 -0300

    fix: similars when not exist

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -23,7 +23,7 @@ const Layout: FC<LayoutProps> = ({ source, nextRecomendation, isFirst }) => {
       <div className="w-full flex justify-end my-3 text-purple md:hidden">
         <Button disabled={fetching} label="Ver siguiente recomendación" size="sm" variant="transparent" onClick={nextRecomendation} />
       </div>
-      {similars && !fetching ? (
+      {similars?.length && !fetching ? (
         <Similars content={similars} isFirst={isFirst} source={source} url={BASE_IMAGE_URL} />
       ) : (
         <Skeleton type="similars" />
