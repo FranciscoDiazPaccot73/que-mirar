29561aeec6bf2eb342bf85dac7d49c5154227861
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2023-06-26 21:08:57 -0300

    add source

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,10 @@ export default class Document extends NextDocument {
       <Html data-theme="dark" lang="es" style={{ background: '#1a202c' }}>
         <Head />
         <div className="absolute top-0 left-0">
-          <video autoPlay loop muted src="/test-bg.mp4" />
+          <video autoPlay loop muted playsInline>
+            <source src="/test-bg.webm" type="video/webm" />
+            <source src="/test-bg.mp4" type="video/mp4" />
+          </video>
         </div>
         <body id="body">
           <Main />
