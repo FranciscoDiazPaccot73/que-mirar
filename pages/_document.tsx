5c32ca9d62bc7e62e72664a3b86ddca471c966c3
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2023-06-26 22:47:13 -0300

    not preload image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,11 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
-import Image from 'next/image';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html data-theme="dark" lang="es" style={{ background: '#1a202c' }}>
         <Head />
-        <Image fill priority alt="bg" className="absolute top-0 left-0" src="/test-bg.webp" />
         <div className="absolute top-0 left-0">
           <video autoPlay loop muted playsInline>
             <source src="/test-bg.webm" type="video/webm" />
