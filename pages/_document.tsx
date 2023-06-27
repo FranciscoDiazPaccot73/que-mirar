6d8a278857f8844f3524ec2bd07cef43da415ec8
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2023-06-26 22:15:34 -0300

    use next image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+import Image from 'next/image';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html data-theme="dark" lang="es" style={{ background: '#1a202c' }}>
-        <Head>
-          <link as="image" href="/test-bg.webp" rel="preload" />
-        </Head>
+        <Head />
+        <Image fill alt="bg" className="absolute top-0 left-0" src="/test-bg.webp" />
         <div className="absolute top-0 left-0">
-          <video autoPlay loop muted playsInline poster="/test-bg.webp">
+          <video autoPlay loop muted playsInline>
             <source src="/test-bg.webm" type="video/webm" />
             <source src="/test-bg.mp4" type="video/mp4" />
           </video>
