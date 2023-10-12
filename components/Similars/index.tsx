a756f0a8bd1c33e83e47e8a3b2d8713afd0269d2
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2023-10-12 18:40:21 -0300

    chore: fix search

--- components/Similars/index.tsx
+++ components/Similars/index.tsx
@@ -1,11 +1,11 @@
-import { FC, useContext } from 'react';
+import { FC, useContext } from "react";
 
-import { ContentInterface } from '@/pages/types';
+import { ContentInterface } from "@/pages/types";
 
-import { PageContext } from '@/context';
-import { getInfo, resetValues } from '@/context/actions';
-import Button from '../Button';
-import ContentBox from './Box';
+import { PageContext } from "@/context";
+import { getInfo, resetValues } from "@/context/actions";
+import Button from "../Button";
+import ContentBox from "./Box";
 
 type SimilarsProps = {
   url: string;
@@ -17,11 +17,14 @@ type SimilarsProps = {
 const Similars: FC<SimilarsProps> = ({ url, content, source, search }) => {
   const {
     dispatch,
-    state: { fetching, selectedTimeframe = 'day' },
+    state: { fetching, selectedTimeframe = "day" },
   } = useContext(PageContext);
-  const deviceName = source === 'movie' ? 'Peliculas' : 'Series';
-  const sortedContent = content.sort((a: ContentInterface, b: ContentInterface) => b.popularity - a.popularity);
-  const text = search === 'trends' ? 'Otras tendencias' : `${deviceName} similares`;
+  const deviceName = source === "movie" ? "Peliculas" : "Series";
+  const sortedContent = content.sort(
+    (a: ContentInterface, b: ContentInterface) => b.popularity - a.popularity
+  );
+  const text =
+    search === "trends" ? "Otras tendencias" : `${deviceName} similares`;
 
   const getTrending = () => {
     if (!fetching) {
@@ -34,14 +37,20 @@ const Similars: FC<SimilarsProps> = ({ url, content, source, search }) => {
   return (
     <div className="mt-20">
       <p className="text-white opacity-90 my-4 pl-4 md:pl-0">{text}</p>
-      <div className="grid gap-6 text-white grid-cols-2 px-1 md:gap-10 md:px-0 md:grid-cols-4">
+      <div className="grid gap-6 text-white grid-cols-2 px-3 md:gap-10 md:px-0 md:grid-cols-4">
         {sortedContent.map((data: ContentInterface) => (
           <ContentBox key={data.id} content={data} source={source} url={url} />
         ))}
       </div>
-      {search === 'trends' && (
+      {search === "trends" && (
         <div className="mt-4 w-full flex justify-center">
-          <Button color="purple" label="Ver mas" size="sm" variant="outline" onClick={getTrending} />
+          <Button
+            color="purple"
+            label="Ver mas"
+            size="sm"
+            variant="outline"
+            onClick={getTrending}
+          />
         </div>
       )}
     </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,11 @@
 /* eslint-disable jsx-a11y/media-has-caption */
-import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html data-theme="dark" lang="es" style={{ background: '#1a202c' }}>
+      <Html data-theme="dark" lang="es" style={{ background: "#1a202c" }}>
         <Head />
-        <div className="absolute top-0 left-0">
-          <video autoPlay loop muted playsInline>
-            <source src="/test-bg.webm" type="video/webm" />
-            <source src="/test-bg.mp4" type="video/mp4" />
-          </video>
-        </div>
         <body id="body">
           <Main />
           <NextScript />
