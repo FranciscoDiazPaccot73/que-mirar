0aac2382d73b4af4c606c3676231a7bd8eef9919
FranciscoDiazPaccot73 <fran.diazpaccot@gmail.com>
2024-01-28 23:05:09 -0300

    feat: add shadcn

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -1,14 +1,25 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import { FC, useContext, useState } from "react";
 
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import { Search } from "lucide-react";
+
 import { PageContext } from "@/context";
 import { resetSearch, search } from "@/context/actions";
 import { ContentInterface } from "@/pages/types";
 
-import Button from "../Button";
+import { Carousel, CarouselContent, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import Input from "../Input";
 import ContentBox from "../Similars/Box";
-import Modal from "../Modal";
 
 type SearchBoxProps = {
   source: string;
@@ -49,53 +60,65 @@ const SearchBox: FC<SearchBoxProps> = ({ source, region }) => {
   };
 
   return (
-    <div>
-      <Button
-        color="gray"
-        icon={<img alt="Open search modal" src="/search.svg" />}
-        label="Buscar"
-        variant="outline"
-        onClick={() => setModalOpen(true)}
-      />
-      <Modal isOpen={modalOpen} resetModal={resetModal}>
-        <div className="relative overflow-hidden h-auto w-full">
-          <p className="font-bold text-xl mb-3 text-white">{`Buscar ${
-            source === "tv" ? "serie" : "pelicula"
-          }`}</p>
-          <div className="w-full mt-5 flex items-center mb-6">
+    <AlertDialog open={modalOpen}>
+      <AlertDialogTrigger asChild>
+        <Button
+          className="p-2 "
+          color="gray"
+          size="sm"
+          variant="ghost"
+          onClick={() => setModalOpen(true)}
+        >
+          <Search className="h-4 w-4 mr-1" /> Buscar
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{`Buscar ${source === "tv" ? "serie" : "pelicula"}`}</AlertDialogTitle>
+        </AlertDialogHeader>
+        <div className="w-full mt-5 flex items-center mb-6">
             <Input
-              placeholder="Buscar por nombre"
+              placeholder="Nombre"
               onChange={handleInputChange}
               onKeyUp={handleKeyUp}
             />
             <Button
-              customClass="ml-3"
+              className="ml-3 h-10"
               disabled={fetching}
-              h="10"
-              label="Buscar"
               onClick={handleSearch}
             />
           </div>
           {searchResult?.length ? (
             <section className="overflow-y-auto max-h-modal-dialog pb-12">
-              <div className="relative overflow-hidden h-auto w-full flex flex-col gap-4">
-                <div className="grid gap-6 text-white grid-cols-1 px-3 md:gap-10 md:px-0 md:grid-cols-3">
-                  {searchResult.map((result: ContentInterface) => (
-                    <article key={result.id} onClick={resetModal}>
+              <div className="relative overflow-hidden h-auto w-full">
+                <Carousel
+                  className="w-full px-10 py-6"
+                  opts={{
+                    align: "center",
+                  }}
+                >
+                  <CarouselContent>
+                    {searchResult.map((result: ContentInterface) => (
                       <ContentBox
+                        key={result.id}
                         content={result}
+                        customAction={resetModal}
                         source={source}
                         url={BASE_IMAGE_URL}
                       />
-                    </article>
-                  ))}
-                </div>
+                    ))}
+                  </CarouselContent>
+                  <CarouselPrevious className="left-6 md:left-0" variant="secondary" />
+                  <CarouselNext className="right-6 md:right-0" variant="secondary" />
+                </Carousel>
               </div>
             </section>
           ) : null}
-        </div>
-      </Modal>
-    </div>
+        <AlertDialogFooter onClick={resetModal}>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
--- components/Similars/Box.tsx
+++ components/Similars/Box.tsx
@@ -1,6 +1,6 @@
 import { FC, useContext, useState } from "react";
-
 import Image from "next/image";
+import { CarouselItem } from "@/components/ui/carousel"
 
 import { PageContext } from "@/context";
 import {
@@ -10,14 +10,16 @@ import {
 } from "@/context/actions";
 import { ContentInterface } from "@/pages/types";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { Card, CardContent } from "../ui/card";
 
 type ContentBoxProps = {
   url: string;
   content: ContentInterface;
   source?: string;
+  customAction?: () => void
 };
 
-const ContentBox: FC<ContentBoxProps> = ({ content, url, source }) => {
+const ContentBox: FC<ContentBoxProps> = ({ content, url, source, customAction }) => {
   const {
     dispatch,
     state: { watchRegion },
@@ -28,18 +30,18 @@ const ContentBox: FC<ContentBoxProps> = ({ content, url, source }) => {
 
   const {
     id,
-    name,
     title,
     vote_average: vote,
-    backdrop_path: backdropPath,
+    poster_path: backdropPath,
     overview,
   } = content;
   const boxClasses =
-    "relative h-full flex flex-col items-center gap-2 cursor-pointer mb-4 md:mb-6 md:hover:scale-105 md:duration-200";
+    "relative h-full flex flex-col items-center gap-2 cursor-pointer mb-4 md:my-3 md:hover:scale-105 md:duration-200 md:basis-1/3 lg:basis-1/4";
 
   const imageUrl = backdropPath;
 
   const handleLoadContent = async () => {
+    if (customAction) customAction()
     if (id) {
       window.scrollTo(0, 0);
       setSimilarToContent(dispatch, content);
@@ -49,41 +51,40 @@ const ContentBox: FC<ContentBoxProps> = ({ content, url, source }) => {
   };
 
   return (
-    <div
-      className={boxClasses}
-      title={overview ?? title}
-      onClick={handleLoadContent}
-    >
-      <Image
-        alt={overview ?? title}
-        blurDataURL={`${url}${backdropPath}`}
-        height={120}
-        placeholder="blur"
-        src={errorLoadingImage ? "/not-image.png" : `${url}${imageUrl}`}
-        width={190}
-        onError={() => setImageError(true)}
-      />
-      {vote ? (
-        <div className="h-10 w-10 absolute top-20 right-0">
-          <CircularProgressbar
-            background
-            maxValue={10}
-            strokeWidth={10}
-            styles={buildStyles({
-              textSize: "18px",
-              pathColor: "#B794F4",
-              textColor: "#B794F4",
-              trailColor: "#B794F412",
-              backgroundColor: "#1a202c",
-            })}
-            text={`${vote.toFixed(2)}`}
-            value={vote}
+    <CarouselItem className={boxClasses} title={overview ?? title} onClick={handleLoadContent}>
+      <Card className="h-[260px] bg-transparent border-purple overflow-hidden">
+        <CardContent className="flex flex-col p-0 h-full">
+          <Image
+            alt={overview ?? title}
+            blurDataURL={`${url}${backdropPath}`}
+            height={120}
+            placeholder="blur"
+            src={errorLoadingImage ? "/not-image.png" : `${url}${imageUrl}`}
+            width={190}
+            onError={() => setImageError(true)}
           />
-        </div>
-      ) : null}
-      <p className="text-lg mt-1">{name || title}</p>
-    </div>
-  );
+          {vote ? (
+            <div className="h-14 w-14 absolute -bottom-4 left-12 md:-bottom-1 md:left-8">
+              <CircularProgressbar
+                background
+                maxValue={10}
+                strokeWidth={10}
+                styles={buildStyles({
+                  textSize: "18px",
+                  pathColor: "#B794F4",
+                  textColor: "#B794F4",
+                  trailColor: "#B794F412",
+                  backgroundColor: "#1a202c",
+                })}
+                text={`${vote.toFixed(2)}`}
+                value={vote}
+              />
+            </div>
+          ) : null}
+        </CardContent>
+      </Card>
+    </CarouselItem>
+  )
 };
 
 ContentBox.defaultProps = {
